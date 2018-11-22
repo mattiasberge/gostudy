7747578335f93643be6ba36c87ab5c387c915575
mattias <mattias.berge@bitrate.se>
2018-11-22 21:48:45 +0100

    cleanup

--- bubblesort.go
+++ bubblesort.go
@@ -14,8 +14,8 @@ func main() {
 	i := 0
 	j := 0
 	iterations := 0
-	swaps := 0
-	swap := 0
+	swaps := 0 // total swaps
+	swap := 0 // if swap within loop cycle or not
 	
 	for i = 0; i < length; i++ {
 		l = append(l, rand.Intn(99))
@@ -24,8 +24,9 @@ func main() {
 	for i = 0; i < len(l); i++ {
 		swap = 0
 		for j = 0; j < len(l); j++ {
+			// if on last element of inner loop, break
 			if j == (len(l) - 1) {
-				continue
+				break
 			}
 			if l[j] > l[j+1] {
 				tmp = l[j+1]
@@ -34,11 +35,12 @@ func main() {
 				
 				swaps++
 				swap = 1
-				
 			}
 			iterations++
 		}
-		if swap == 0 {
+
+		// no change for entire run, looks like we're done
+		if swap == 0 { 
 			break
 		}
 		
